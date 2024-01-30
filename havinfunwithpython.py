9b0497be016363949dabaeaff0c3013372cebe69
Asegeas <egeland01@allegheny.edu>
2024-01-30 14:45:28 -0500

    hecker

--- havinfunwithpython.py
+++ havinfunwithpython.py
@@ -1,6 +1,24 @@
 from rich.console import Console
 import random
+import json
+import time
+import sys
+
+def slowPrint(string, speed=0.05):
+    loop = 0
+    for char in string:
+        if f"{char}{string[string.index(char) + 1]}" == "\n":
+            loop += 1
+            console.print("\n")
+        if loop == 1:
+            loop -= 1
+        else:
+            console.print(f"{char}", style="rgb(32,194,14)", end='') # Print out the hacker text in hacker green
+            sys.stdout.flush()
+            time.sleep(speed)
+
 console = Console()
+
 
 def main():
     count = 0 # variable for controlling the loops of while 
@@ -8,16 +26,21 @@ def main():
 
     console.print("You are accessing the mainframe.. continue(y/n):")
     duter = input("") # takes a number
-
+    with open("scripts.JSON", 'r') as scpt:
+        object = json.load(scpt)
     try:
         duter = int(duter)
 
     except:
         duter = 64 # default value of 'duter' if user enters non numerical units
     while count <= duter:
-        random.randrange(0, 4)
+        rnd = random.randrange(0, 7)
         count += 1
-        console.print(f"{thing}", style="rgb(32,194,14)")
+        put = object['initialHC'][rnd]
+        slowPrint(put, 0.01) # Print out slowly from here: https://stackoverflow.com/questions/66913084/print-slowly-in-terminal-python
+        rnd2 = random.randrange(0, 7)
+        put2 = object['secondaryHC'][rnd2]
+        slowPrint(put2, 0.01) # Print out the hacker text in hacker green
 
 if __name__ == '__main__':
     main()
